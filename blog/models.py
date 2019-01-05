201623a20d44e4a9202236b88f45a6abeaab3271
DD <daij7@uw.edu>
2019-01-04 17:11:51 -0800

    finish all the functions, still need to polish it with css

--- blog/models.py
+++ blog/models.py
@@ -9,8 +9,14 @@ from django.db import models
 class Blog(models.Model):
     title = models.CharField(max_length=100) #maximum length for CharField is 255
     pub_date = models.DateTimeField()
-    body = models.TextField(max_length=1000)
+    body = models.TextField()
     image = models.ImageField(upload_to='imgs/')
+
+    def pub_date_pretty(self):
+        return self.pub_date.strftime('%c')
+
+    def __str__(self): #set the blog id in admin same as the title
+        return self.title
 
 #add the blog app to the settings
 
--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404 #get detailed blog page if exists and 404 if not
+from .models import Blog
 
 # Create your views here.
+def allblogs(request):
+    allblogs = Blog.objects.order_by('-pub_date') #biggest date first = newest, hence - sign
+    return render(request, 'blog/allblogs.html', {'allblogs' : allblogs})
+
+def detail(request, blog_id):
+    blogdetail = get_object_or_404(Blog, pk=blog_id) #pk: primary key
+    return render(request, 'blog/detail.html', {'blog':blogdetail})
